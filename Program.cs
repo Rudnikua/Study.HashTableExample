ecf3bdc4380f51a934544ac742db1e4a3b2f8935
Rudnikua <myroslavrudnikua@gmail.com>
2025-12-13 15:14:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HashTableExample
+{
+    public class HashTable {
+        private string[] _table;
+        private int _size;
+        public HashTable(int size) {
+            _size = size;
+            _table = new string[size];
+        }
+
+        private int GetHash(string name) {
+            int ASCIIsum = 0;
+            Console.Write($"Calculating hash for '{name}': ");
+
+            foreach (char letter in name) {
+                int ASCIIcode = (int)letter;
+                ASCIIsum += ASCIIcode;
+                Console.Write($"'{letter}'({ASCIIcode}) + ");
+            }
+
+            int index = ASCIIsum % _size;
+
+            Console.WriteLine($"'{name}' is stored at index: {index} ");
+
+            return index;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
